216ef500672ccc89a785708e3a0ed03590cb0f99
Arman-ATI <altafiarman5@gmail.com>
2024-05-01 20:25:48 +0330

    wly: init rising

--- lineage_wly.mk
+++ lineage_wly.mk
@@ -14,6 +14,36 @@ $(call inherit-product, device/oneplus/wly/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Rising
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    RisingChipset="Snapdragon 8 Gen 1" \
+    RisingMaintainer="Arman ATI"
+
+# Blur
+TARGET_ENABLE_BLUR := true
+
+# UDFPS
+TARGET_HAS_UDFPS := true
+
+# Launchers
+TARGET_DEFAULT_PIXEL_LAUNCHER := true
+TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true
+
+# Extra stuff
+TARGET_PREBUILT_BCR := true
+
+# Camera
+PRODUCT_NO_CAMERA := true
+TARGET_USES_OPLUS_CAMERA := true
+TARGET_PREBUILT_GOOGLE_CAMERA := true
+
+# Gapps
+WITH_GMS := true
+
+# ADB root
+ALLOW_ADBD_ROOT := true
+ALLOW_LOCAL_PROP_OVERRIDE := true
+
 PRODUCT_NAME := lineage_wly
 PRODUCT_DEVICE := wly
 PRODUCT_MANUFACTURER := OnePlus
