60e9720e7889e9138388d46537babd0e58ff383d
armanati <altafiarman5@gmail.com>
2024-07-13 02:09:27 +0330

    Revert "wly: Build DTB/O from sm8450-devicetrees"
    This reverts commit ba95742058d9a20858961b831516c87db732a3e7.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,7 +12,10 @@ DEVICE_PATH := device/oneplus/wly
 # Display
 TARGET_SCREEN_DENSITY := 450
 
-# DTB
+# DTB/O
+BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilts/dtbo.img
+TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilts/dtb.img
+BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
 TARGET_KERNEL_CONFIG += vendor/oplus/wly.config
 
 # HIDL
