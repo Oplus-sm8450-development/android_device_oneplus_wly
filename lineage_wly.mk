6bc814722b74d853dad7a021a642ea7779e11eda
danya2271 <dany2271@yandex.ru>
2024-12-14 16:50:16 +0500

    Switch to lineage

--- lineage_wly.mk
+++ lineage_wly.mk
@@ -14,11 +14,6 @@ $(call inherit-product, device/oneplus/wly/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Rising
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    RisingChipset="Snapdragon 8 Gen 1" \
-    RisingMaintainer="Arman ATI"
-
 # Blur
 TARGET_ENABLE_BLUR := true
 
@@ -36,9 +31,6 @@ TARGET_PREBUILT_BCR := true
 PRODUCT_NO_CAMERA := true
 TARGET_USES_OPLUS_CAMERA := true
 TARGET_PREBUILT_GOOGLE_CAMERA := true
-
-# Gapps
-WITH_GMS := true
 
 # ADB root
 ALLOW_ADBD_ROOT := true
